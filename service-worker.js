7063a5f4edf1844ed4b7cf26d921545086bdcb81
Yash <yash382010@gmail.com>
2026-09-20 22:51:10 +0530

    Force fresh BoardFlow cache

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,25 @@
-const C="boardflow-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE="boardflow-v3-fresh";
+const FILES=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
+
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    )).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
