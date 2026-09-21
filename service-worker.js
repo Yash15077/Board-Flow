629bb7c045250d4c0f8cfc26c1f392c1296114ca
Yash <yash382010@gmail.com>
2026-09-21 13:58:52 +0530

    BoardFlow Final V7 Study OS

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,41 @@
-const CACHE="boardflow-v4";
-const FILES=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
+const CACHE="boardflow-final-v7";
+
+const FILES=[
+"./",
+"./index.html",
+"./styles.css",
+"./app.js",
+"./manifest.json",
+"./icon.svg"
+];
+
+self.addEventListener("install",e=>{
+ self.skipWaiting();
+ e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(FILES))
+ );
+});
+
+self.addEventListener("activate",e=>{
+ e.waitUntil(
+  caches.keys().then(keys=>
+   Promise.all(
+    keys
+     .filter(k=>k!==CACHE)
+     .map(k=>caches.delete(k))
+   )
+  ).then(()=>self.clients.claim())
+ );
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  fetch(e.request)
+   .then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy));
+    return r;
+   })
+   .catch(()=>caches.match(e.request))
+ );
+});
